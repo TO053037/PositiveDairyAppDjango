71201f1ba1174db19ebe7ca0921dde5b8bac6fa2
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-09-26 13:18:34 +0900

    create signup, login and logout func

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import AuthenticationForm
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email',)
--- dairyApp/views.py
+++ dairyApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello world")
+def index(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        return HttpResponse("Yes Hello world")
+    else:
+        return HttpResponse("No Hello World")
