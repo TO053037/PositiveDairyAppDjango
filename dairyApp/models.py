246fd3468498858c66dfd05295ce82897a48616c
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-09-29 16:41:55 +0900

    exchange model name to user_object from user_id

--- dairyApp/models.py
+++ dairyApp/models.py
@@ -1,5 +1,4 @@
 from tabnanny import verbose
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -9,7 +8,7 @@ class DairyContent(models.Model):
         max_length=300, verbose_name='楽しかったことを記入しよう', null=True, blank=True)
     date = models.DateField(null=False, blank=True)
     ranking = models.IntegerField(null=False, blank=True)
-    user_id = models.ForeignKey(
+    user_object = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, related_name="dairyContent")
     
 
@@ -17,7 +16,7 @@ class DairyPicture(models.Model):
     title = models.CharField(max_length=60, verbose_name='写真のタイトル')
     date = models.DateField(null=False, blank=True)
     image = models.ImageField(upload_to='images')
-    user_id = models.ForeignKey(
+    user_object = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, related_name="dairyPicture")
     
     
