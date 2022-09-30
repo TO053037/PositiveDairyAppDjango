9d41af976bc1c3960c00c09f32e9cfbf1b01ee6c
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-09-30 13:03:17 +0900

    create get method

--- dairyApp/urls.py
+++ dairyApp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('post_dairy_content', views.post_dairy_content,
-         name='post_dairy_content')
+         name='post_dairy_content'),
+    path('get_dairy_content/', views.get_dairy_content,
+         name='get_dairy_content'),
 ]
--- dairyApp/views.py
+++ dairyApp/views.py
@@ -12,19 +12,30 @@ import json
 @login_required
 def index(request: HttpRequest):
     print(request.user.email)
-    return render(request, 'dairyApp/index.html')
+    context = {}
+    return render(request, 'dairyApp/index.html', context)
 
 
 @login_required
 def post_dairy_content(request: HttpRequest):
     if request.method == 'POST':
         dairy_content = {
-                     'content': request.POST.get('content'),
-                     'date': request.POST.get('date'),
-                     'ranking': request.POST.get('ranking'),
-                        }
-        DairyContent.objects.create(content=dairy_content['content'], user_id=request.user,
+            'content': request.POST.get('content'),
+            'date': request.POST.get('date'),
+            'ranking': request.POST.get('ranking'),
+        }
+        DairyContent.objects.create(content=dairy_content['content'], user_object=request.user,
                                     ranking=dairy_content['ranking'],
                                     date=dairy_content['date'])
         return HttpResponse(json.dumps(dairy_content), content_type="application/json")
+    raise Http404('not working')
+
+
+@login_required
+def get_dairy_content(request: HttpRequest):
+    if request.method == 'GET':
+        date = request.GET.get('date')
+        ranking = request.GET.get('ranking')
+        content = DairyContent.objects.get(user_object=request.user, ranking=ranking, date=date)
+        return HttpResponse(json.dumps(content), content_type="applications/json")
     raise Http404('not working')
