d26060b325ff3ee1446ee7abbac1507f36112fc0
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-09-30 13:12:53 +0900

    create admin dairyApp

--- dairyApp/admin.py
+++ dairyApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DairyContent
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user_object', 'content', 'ranking', 'date')
+
+
+admin.site.register(DairyContent, AuthorAdmin)
