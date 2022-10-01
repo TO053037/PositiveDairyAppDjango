361b7ddaf7d07ebac8534b2f80eb2bb4d80d6979
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-10-01 14:09:16 +0900

    debug onload event

--- static/js/onloadEvent.js
+++ static/js/onloadEvent.js
@@ -1,15 +1,8 @@
-function getDairyContentToday() {
+function onloadEvent() {
     const date = createNewClass()
     getDairyContent(date, 1)
     getDairyContent(date, 2)
     getDairyContent(date, 3)
-}
-
-function onloadEvent() {
-    console.log('in first onload');
-    onloadEvent();
-    console.log('in onloadEvent');
-    getDairyContentToday();
 }
 
 window.onload = onloadEvent;
