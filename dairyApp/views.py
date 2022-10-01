d1673a316afa6b3acb1b1967da022397fe679278
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-10-01 13:49:32 +0900

    except DoseNotExist error

--- dairyApp/views.py
+++ dairyApp/views.py
@@ -7,7 +7,9 @@ import datetime
 import json
 
 
-# Create your views here.
+def create_date_obj(date: str) -> datetime:
+    return datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
 
 @login_required
 def index(request: HttpRequest):
@@ -22,22 +24,24 @@ def index(request: HttpRequest):
 @login_required
 def post_dairy_content(request: HttpRequest):
     if request.method == 'POST':
-        date_object = datetime.datetime.strptime(request.POST.get('date'), '%Y-%m-%d').date()
-        print(date_object)
         dairy_content = {
             'content': request.POST.get('content'),
             'date': request.POST.get('date'),
             'ranking': request.POST.get('ranking'),
         }
 
-        print(dairy_content['date'])
-        DairyContent.objects.create(content=dairy_content['content'], user_object=request.user,
-                                    ranking=dairy_content['ranking'],
-                                    date=datetime.datetime.strptime(dairy_content['date'], '%Y-%m-%d').date())
+        try:
+            dairy_content_obj = DairyContent.objects.get(user_object=request.user,
+                                                         date=create_date_obj(dairy_content['date']),
+                                                         ranking=dairy_content['ranking'], )
+            dairy_content_obj.content = dairy_content['content']
+            dairy_content_obj.save()
+        except DairyContent.DoesNotExist:
+            DairyContent.objects.create(user_object=request.user, date=create_date_obj(dairy_content['date']),
+                                        ranking=dairy_content['ranking'], content=dairy_content['content'])
         return JsonResponse({
             'dairyContent': dairy_content
-        }
-        )
+        })
     raise Http404('not working')
 
 
@@ -48,7 +52,8 @@ def get_dairy_content(request: HttpRequest):
         print(date)
         ranking = request.GET.get('ranking')
         content = DairyContent.objects.get(user_object=request.user, ranking=ranking,
-                                           date=datetime.datetime.strptime(request.GET.get('date'), '%Y-%m-%d').date()).content
+                                           date=datetime.datetime.strptime(request.GET.get('date'),
+                                                                           '%Y-%m-%d').date()).content
         print(content)
         return JsonResponse({
             'content': content
