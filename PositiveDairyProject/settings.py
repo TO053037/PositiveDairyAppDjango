1b5e435abf9be3a15211a2c979810c64f0b3aa14
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-11-02 10:43:19 +0900

    heroku: config test

--- PositiveDairyProject/settings.py
+++ PositiveDairyProject/settings.py
@@ -101,7 +101,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -155,4 +154,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ["https://sheltered-atoll-98800.herokuapp.com"]
 
 import django_heroku
+
 django_heroku.settings(locals())
